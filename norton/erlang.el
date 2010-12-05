3ae2b95ed870a144ebc5f4316fbf8cacf83aaa7c
Joseph Wayne Norton <norton@alum.mit.edu>
2010-12-05 21:56:16 +0900

    rework erlang setup

--- norton/erlang.el
+++ norton/erlang.el
@@ -1,22 +1,27 @@
 ;; This is needed for Erlang mode setup
+(defun erlang-setup ()
+  (if (file-exists-p "/usr/local/gemini/ert/R14B/lib/erlang")
+      (setq erlang-basefolder "/usr/local/gemini/ert/R14B/lib/erlang")
+    (if (file-exists-p "/usr/local/lib/erlang")
+        (setq erlang-basefolder "/usr/local/lib/erlang")))
+  (if erlang-basefolder
+      (progn
+        (let ((lib-folder (concat erlang-basefolder "/lib")))
+          (dolist (fldr (directory-files lib-folder))
+            (if (and
+                 (> (length fldr) 6)
+                 (string= (substring fldr 0 6) "tools-"))
+                (setq erlang-tools-dir (concat lib-folder (concat "/" fldr)))))
+          (setq load-path (cons (concat erlang-tools-dir "/emacs") load-path))
+          (setq erlang-root-dir erlang-basefolder)
+          (setq exec-path (cons (concat erlang-basefolder "/bin")  exec-path))
+          (require 'erlang-start)
+          (add-hook 'erlang-mode-hook
+                    (lambda ()
+                      ;; when starting an Erlang shell in Emacs, the node
+                      ;; name by default should be "emacs"
+                      (setq inferior-erlang-machine-options '("-sname" "emacs"))
+                      ;; add Erlang functions to an imenu menu
+                      (imenu-add-to-menubar "imenu")))))))
 
-(defun wild (dir stem)
-  "returns the last (alphabetically) filename that matches dir/stem*."
-  (car
-   (reverse
-    (sort
-     (let (value)
-       (dolist (element (file-name-all-completions stem dir) value)
-         (setq value (cons (concat dir element) value)))) 'string-lessp))))
-
-(setq erlang-root-dir "/usr/local/lib/erlang/")
-(setq load-path (cons (concat (wild (concat erlang-root-dir "/lib/") "tools-") "emacs") load-path))
-(setq exec-path (cons (concat erlang-root-dir "/bin") exec-path))
-(require 'erlang-start)
-(add-hook 'erlang-mode-hook
-	  (lambda ()
-	    ;; when starting an Erlang shell in Emacs, the node
-	    ;; name by default should be "emacs"
-	    (setq inferior-erlang-machine-options '("-sname" "emacs"))
-	    ;; add Erlang functions to an imenu menu
-	    (imenu-add-to-menubar "imenu")))
+(erlang-setup)
